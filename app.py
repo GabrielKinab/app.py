2367994d5981e1f9109622ccd251675040695067
GabrielKinab <129771579+GabrielKinab@users.noreply.github.com>
2024-04-01 12:08:18 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+
+print("Bem vindo ao programa Expresso")
+
+print("Programa Expresso")
+
+print("1-Cadastrar restaurante")
+
+print("2-Listar Restaurante")
+
+print("3-Ativar Restaurante")
+
+print("Sair do Aplicativo")
+
+opcao_digitada= int(input("Digite a opção digitada"))
+if (opcao_digitada == 1):
+print("você escolheu cadastrar restaurante")
+
+elif(opcao_digitada == 2):
+print("você escolheu listar restaurante")
+
+elif(opcao_digitada == 3):
+print("você escolheu ativar restaurante")
+
+elif(opcao_digitada == 4):
+print("você escolheu sair do aplicativo")
+
+else:
+print("você encerrou o aplicativo")
+
+
+
+
+
+
+
